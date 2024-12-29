5c59bdc78a96ab299a253313aada8180c73bf9a7
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-29 17:46:24 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,7 +1,7 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../TheApi'))
-# sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath('..'))
 
 project = 'TheApix'
 author = 'Vivekkumar-IN'
