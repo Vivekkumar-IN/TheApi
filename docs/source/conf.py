1cdb5c1ba862cef7270dd59a4ff4ff71a3cff8f0
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-29 14:26:43 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,31 +1,29 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'TheApi'
-copyright = '2024, Vivekkumar-in'
-author = 'Vivekkumar-in'
+project = 'TheApix'
+author = 'Vivekkumar-IN'
 release = '1.2.7'
-
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+copyright = "2024-present, Vivek"
 
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.viewcode',
 ]
 
 templates_path = ['_templates']
 exclude_patterns = []
+html_title = "TheApix"
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = "furo"
+html_copy_source = False
 html_static_path = ['_static']
+html_css_files = [
+    "css/custom.css",
+]
+
+html_theme_options = {
+    "navigation_with_keys": True,
+}
