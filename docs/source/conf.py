20480c347411b6d1703ef9612d18902dacb921b9
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-31 14:05:10 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ html_theme_options = {
         },
         {  # Github logo
             "name": "GitHub",
-            "url": f"https://github.com/TelegramPlayGround/pyrogram/tree/{commit_id}",
+            "url": f"https://github.com/Vivekkumar-IN/TheApi/tree/dev",
             "html": (
                 '<svg stroke="currentColor" fill="currentColor" stroke-width="0" '
                 'viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 '
