5a3613166c6150676fa049ba964699c9ef4221a5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-12-31 18:17:05 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,8 +30,8 @@ html_theme = "furo"
 html_copy_source = False
 html_static_path = ["_static"]
 html_css_files = ["css/pyrogram.css"]
-html_logo = html_static_path + "/img/TheTeamVivek.png"
-html_favicon = html_static_path + "/img/TheTeamVivek.ico"
+html_logo = html_static_path[0] + "/img/TheTeamVivek.png"
+html_favicon = html_static_path[0] + "/img/TheTeamVivek.ico"
 
 html_theme_options = {
     "navigation_with_keys": True,
