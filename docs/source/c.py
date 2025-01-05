fe974672af38c6481e2a95c07a335ba7bd37d287
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-01-05 12:05:05 +0530

    Update c.py

--- docs/source/c.py
+++ docs/source/c.py
@@ -60,7 +60,7 @@ def generate_method_docs(cls, method_name, file_path):
 from TheApi import SaavnAPI
 
 
-file_path = os.path.join(os.path.abspath(os.sep), "docs", "source", "test.rst")
+file_path = "source/test.rst"
 print(file_path)
 
 generate_method_docs(SaavnAPI, "search", file_path)
