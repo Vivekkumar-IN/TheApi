cea43281e5b020b481b8096b2867070ec48a9cde
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-01-05 12:34:43 +0530

    Update c.py

--- docs/source/c.py
+++ docs/source/c.py
@@ -1,6 +1,5 @@
 import os
 import inspect
-
 import docstring_parser
 
 
@@ -14,31 +13,31 @@ def generate_method_docs(cls, method_name, file_path):
     is_async = inspect.iscoroutinefunction(method)
     func_type = "async" if is_async else "sync"
     func_signature = inspect.signature(method)
-    func_return_type = method.__annotations__.get("return", "None")
+    func_return_type = method.__annotations__.get("return", None)
     docstring = inspect.getdoc(method) or ""
     parsed_docstring = docstring_parser.parse(docstring)
 
+    # Generate method documentation
     doc_string = f"""
 {method_name}
 {'=' * len(method_name)}
 
-{func_type} {cls.__name__}.{method_name}{func_signature} -> {func_return_type}::
-
-   {parsed_docstring.short_description or "No description provided."}
+``{func_type} {cls.__name__}.{method_name}{func_signature}``::
+   {parsed_docstring.short_description or "".strip()}
 
 **Parameters:**
 """
     for param in parsed_docstring.params:
         param_name = param.arg_name
-        param_type = param.type_name
+        param_type = param.type_name or "Unknown"
         doc_string += f"    - **{param_name}** (``{param_type}``): {param.description or 'No description'}.\n"
 
-    doc_string += f"""
+    if func_return_type and func_return_type != "None":
+        doc_string += f"""
 
 **Returns:**
 
-    **{func_return_type}**: {parsed_docstring.returns or 'No return description.'}
-
+    **{func_return_type}**: {parsed_docstring.returns.description if parsed_docstring.returns else 'No return description.'}
 """
 
     if parsed_docstring.raises:
@@ -49,8 +48,7 @@ def generate_method_docs(cls, method_name, file_path):
     if parsed_docstring.examples:
         doc_string += "\n**Examples:**\n\n"
         for example in parsed_docstring.examples:
-            doc_string += f"    - {example}\n"
-
+            doc_string += f"    - {example.description}\n"
     os.makedirs(os.path.dirname(file_path), exist_ok=True)
     with open(file_path, "w") as file:
         file.write(doc_string.strip())
@@ -59,8 +57,5 @@ def generate_method_docs(cls, method_name, file_path):
 
 from TheApi import SaavnAPI
 
-
 file_path = "source/test.rst"
-print(file_path)
-
 generate_method_docs(SaavnAPI, "search", file_path)
